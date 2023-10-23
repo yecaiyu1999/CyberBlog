39cde9188fcddaff6cb6ff5a69d4e5694789fc29
YeCaiYu <yecaiyu@foxmail.com>
2023-10-23 17:14:57 +0800

    done

---
+++ themes/heo/components/AnalyticsCard.js
@@ -0,0 +1,43 @@
+import CONFIG from '../config'
+
+/**
+ * 博客统计卡牌
+ * @param {*} props
+ * @returns
+ */
+export function AnalyticsCard(props) {
+  const targetDate = new Date(CONFIG.SITE_CREATE_TIME)
+  const today = new Date()
+  const diffTime = today.getTime() - targetDate.getTime() // 获取两个日期之间的毫秒数差值
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) // 将毫秒数差值转换为天数差值
+
+  const { postCount } = props
+  return <>
+        <div className='text-md flex flex-col space-y-1 justify-center px-3'>
+            <div className='inline'>
+                <div className='flex justify-between'>
+                    <div>文章数量:</div>
+                    <div>{postCount}</div>
+                </div>
+            </div>
+            <div className='inline'>
+                <div className='flex justify-between'>
+                    <div>建站天数:</div>
+                    <div>{diffDays} 天</div>
+                </div>
+            </div>
+            <div className='hidden busuanzi_container_page_pv'>
+                <div className='flex justify-between'>
+                    <div>浏览量👀:</div>
+                    <div className='busuanzi_value_page_pv' />
+                </div>
+            </div>
+            <div className='hidden busuanzi_container_site_uv'>
+                <div className='flex justify-between'>
+                    <div>访客数👦:</div>
+                    <div className='busuanzi_value_site_uv' />
+                </div>
+            </div>
+        </div>
+        </>
+}
---
+++ themes/hexo/components/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import BLOG from '@/blog.config'
+
+const Footer = ({ title }) => {
+  const d = new Date()
+  const currentYear = d.getFullYear()
+  const copyrightDate = (function() {
+    if (Number.isInteger(BLOG.SINCE) && BLOG.SINCE < currentYear) {
+      return BLOG.SINCE + '-' + currentYear
+    }
+    return currentYear
+  })()
+
+  return (
+    <footer
+      className='relative z-10 dark:bg-black flex-shrink-0 bg-hexo-light-gray justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm p-6'
+    >
+      <i className='fas fa-copyright' /> {`${copyrightDate}`} <span><i className='mx-1 animate-pulse fas fa-heart'/> <a href={BLOG.LINK} className='underline font-bold  dark:text-gray-300 '>{BLOG.AUTHOR}</a>.<br/>
+
+      {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{BLOG.BEI_AN}</a><br/></>}
+
+      <span className='hidden busuanzi_container_site_pv'>
+      👀<span className='px-1 busuanzi_value_site_pv'> </span>  </span>
+      <span className='pl-2 hidden busuanzi_container_site_uv'>
+      👦<span className='px-1 busuanzi_value_site_uv'> </span> </span>
+       </span><br/>
+    </footer>
+  )
+}
+
+export default Footer
